e9615d82d7bfeffdb7b291a2c6f9cf40abb5862e
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2026-04-23 13:01:42 +0530

    Added HitRecord class

---
+++ headers/hit_record.h
@@ -0,0 +1,24 @@
+#ifndef QRTC_HIT_RECORD_H
+#define QRTC_HIT_RECORD_H
+
+#include "../include/qrtc.h"
+#include "vec3.h"
+#include "ray.h"
+
+typedef struct HitRecord
+{   
+    Point3 p;
+    Vec3 normal;
+    float t;
+    bool front_face;
+
+} HitRecord;
+
+void SetHitRecorodFaceNormal(HitRecord* h_rec, const Ray* r, const Vec3* outward_normal)
+{
+    h_rec->front_face = DotProductVec3(&(r->dir), outward_normal) < 0.0f;
+    h_rec->normal = h_rec->front_face ? *outward_normal : NegateVec3(outward_normal);
+    return;
+}
+
+#endif
